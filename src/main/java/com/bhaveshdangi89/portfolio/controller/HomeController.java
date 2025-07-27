9b8d73700c7f6144432e67e958facd0e39c62d33
Bhavesh Dangi <bhaveshdan76@gmail.com>
2025-07-27 23:49:09 +0530

    Update loader animation and other improvements

---
+++ src/main/java/com/bhaveshdangi89/portfolio/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.bhaveshdangi89.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"","/","/home"})
+    public String showHomePage() {
+        return "Home";
+
+    }
+}
